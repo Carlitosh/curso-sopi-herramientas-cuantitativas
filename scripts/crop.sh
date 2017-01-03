cea656c31b4daeececba1ab1b36d600014fcf862
Francisco Nemiña <fnemina@gmail.com>
2017-01-03 16:42:09 -0300

    Cambiamos la zona de estudio para el curso de 2017. Va a ser el departamento de 12 de Octubre, Chaco, durante el periodo de 1988-2011 utilizando imagenes landsat 5. Se agregan las imagenes ya recortadas

---
+++ scripts/crop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in $(ls *.TIF)
+do
+    gdalwarp -overwrite -cutline ../material/imagenes/roi.shp -crop_to_cutline -s_srs EPSG:32620 -t_srs EPSG:32720 "$i" "crop$i"
+    rm "$i"
+    mv "crop$i" "$i"
+done
+
+
