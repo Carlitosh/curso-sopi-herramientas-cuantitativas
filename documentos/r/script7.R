9dd6282263c95d77cd46de85b6ecfe4f68359400
Francisco Nemiña <fnemina@gmail.com>
2017-01-17 14:08:10 -0300

    Se sigue explorando las herramientas de analisis temporal sobre rasters

--- documentos/r/script7.R
+++ documentos/r/script7.R
@@ -3,13 +3,27 @@ library(raster)
 library(rasterVis)
 library(RStoolbox)
 library(bfastSpatial)
-
+library(greenbrown)
 cols <- colorRampPalette(brewer.pal(9,"YlGn"))
 
 # Cargamos las bandas de modis
-ndvi.bands <- list.files("raster_data/MOD13Q1/EVI/", pattern = "*.tif$", full.names = TRUE)
-ndvi.stack <- timeStackMODIS(ndvi.bands)
+evi.bands <- list.files("raster_data/MOD13Q1/EVI/", pattern = "*.tif$", full.names = TRUE)
+evi.stack <- timeStackMODIS(evi.bands)
 
-names(ndvi.stack) <- ndvi.stack@z$time
+#names(evi.stack) <- ndvi.stack@z$time
 
-levelplot(ndvi.stack, layers = 1:4,col.regions=cols)
+levelplot(evi.stack, layers = 1:4,col.regions=cols)
+
+# Lleno los NA
+evi.aprox <- approxNA(evi.stack)
+
+# Convertimos la imagen a numeros entre -1 y 1
+evi.aprox <- evi.aprox/1e4
+evi.aprox@z <- evi.stack@z
+
+
+evi.pca <- rasterPCA(evi.aprox)
+
+writeRaster(evi.aprox, "raster_data/aprox",format="ENVI")
+
+trend.evi <- TrendRaster(evi.aprox, start=c(2000,3), freq = 23)
