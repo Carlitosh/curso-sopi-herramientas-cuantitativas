26a1f39aceb5be3d3f4e1535eb7e2aa99f124718
Francisco Nemiña <fnemina@gmail.com>
2017-01-19 13:52:03 -0300

    Se agregan algunas partes de transformacion de series temporales al script de la guia 4

--- documentos/r/script4.R
+++ documentos/r/script4.R
@@ -1,34 +1,60 @@
 library(raster)
 library(RStoolbox)
-
+library(bfastSpatial)
+library(rts)
 # Abrimos la imagen landsat 8
-xml.2015 <- readMeta("raster_data/LC82300772015071/LC82300772015071LGN00.xml")
-ref.2015 <- stackMeta(xml.2015, quantity = "sre")
-scaleF <- getMeta(ref.2015, xml.2015, what = "SCALE_FACTOR")
-ref.2015 <- ref.2015 * scaleF
-ref.2015 <- ref.2015[[-1,]]
-names(ref.2015) <- c("blue","green","red","nir","swir1","swir2")
+xml.2016 <- readMeta("raster_data/LC82240782016304/LC82240782016304LGN00.xml")
+ref.2016 <- stackMeta(xml.2016, quantity = "sre")
+scaleF <- getMeta(ref.2016, xml.2016, what = "SCALE_FACTOR")
+ref.2016 <- ref.2016 * scaleF
+ref.2016 <- ref.2016[[-1,]]
+names(ref.2016) <- c("blue","green","red","nir","swir1","swir2")
 
 # Abrimos la imagen landsat 5
-xml.1992 <- readMeta("raster_data/LT52300771992104/LT52300771992104CUB00.xml")
-ref.1992 <- stackMeta(xml.1992, quantity = "sre")
-scaleF <- getMeta(ref.1992, xml.2015, what = "SCALE_FACTOR")
-ref.1992 <- ref.1992 * scaleF
-names(ref.1992) <- c("blue","green","red","nir","swir1","swir2")
+xml.2000 <- readMeta("raster_data/LE72240782000188/LE72240782000188EDC00.xml")
+ref.2000 <- stackMeta(xml.2000, quantity = "sre")
+scaleF <- getMeta(ref.2000, xml.2016, what = "SCALE_FACTOR")
+ref.2000 <- ref.2000 * scaleF
+names(ref.2000) <- c("blue","green","red","nir","swir1","swir2")
 
 # Calculamos las transformadas por PCA de la imagen
 
-pca.2015 <- rasterPCA(ref.2015)
-summary(pca.2015$model)
-loadings(pca.2015$model)
-plot(pca.2015$map)
+pca.2016 <- rasterPCA(ref.2016)
+summary(pca.2016$model)
+loadings(pca.2016$model)
+plot(pca.2016$map)
 
 # PCA de apilado
-delta <- stack(ref.2015, ref.1992)
-names(delta) <- c("blue.2015","green.2015","red.2015","nir.2015","swir1.2015","swir2.2015",
-                  "blue.1992","green.1992","red.1992","nir.1992","swir1.1992","swir2.1992")
+delta <- stack(ref.2016, ref.2000)
+names(delta) <- c("blue.2016","green.2016","red.2016","nir.2016","swir1.2016","swir2.2016",
+                  "blue.2000","green.2000","red.2000","nir.2000","swir1.2000","swir2.2000")
+pca.delta <- rasterPCA(delta)
 plot(pca.delta$map,1:4)
+# Serie temporal
+ndvi.list <- list.files("raster_data/MOD13Q1/NDVI/", pattern = "*.tif$", full.names = TRUE)
+ndvi.stack <- stack(ndvi.list)
+ndvi.stack <- ndvi.stack/1e4
+ndvi.stack <- approxNA(ndvi.stack)
+# Interpolamos los valores faltantes 
+fechas <- strptime(as.numeric(gsub(".NDVI","",gsub("MOD13Q1.A",replacement = "",names(ndvi.stack))))/1e3,"%Y.%j")
+fechas <- as.POSIXct(fechas)
+
+# Creamos el objeto temporal
+ndvi.time <- rts(ndvi.stack, fechas)
+
+# Analizamos el promedio y el desvio por año
+ndvi.mean <- apply.yearly(ndvi.time, FUN=mean)
+ndvi.sd <- apply.yearly(ndvi.time, FUN=sd)
+
+# Analizamos la serie temporal en un pixel
+p5000 <- extract(ndvi.time, y=5000)
+plot(p5000)
+
+# Analizamos el PCA
+ndvi.pca <- rasterPCA(ndvi.stack)
+plot(ndvi.pca$map,1:4)
 
 # Transformacion por TSC
-tsa.2015 <- tasseledCap(ref.2015,sat="Landsat8OLI")
-names(tsa.2015) <- c("Brigthness","Greenness","Wetness")
+tsa.2016 <- tasseledCap(ref.2016,sat="Landsat8OLI")
+names(tsa.2016) <- c("Brigthness","Greenness","Wetness")
+
