ea26f7e85d397ebdb1e5a7a028e903be3de806dd
Francisco Nemiña <fnemina@gmail.com>
2017-05-03 14:38:22 -0300

    Corregimos el script de la clase 2 para agregar la libreria rasterVis

--- material/example_scripts/clase-2.R
+++ material/example_scripts/clase-2.R
@@ -33,7 +33,9 @@ sdos.2000 <- radCor(dn.2000, metaData = meta.2000,
 
 sdos.2000
 
-stack(toa.2000,sdos.2000)
+library(rasterVis)
+
+toa.boa <- stack(toa.2000,sdos.2000)
 
 B1 <- densityplot(~B1_tre+B1_sre, data=toa.boa, xlab="Reflectancia",
                   ylab="", main="Banda azul", plot.points=FALSE, xlim=c(0,0.3),
